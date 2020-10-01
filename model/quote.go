99ef75a07838e85fa9e7a3a9ae7acb9b62f62ab7
louislaugier <l_laugier@etu-webschoolfactory.fr>
2020-10-01 14:37:17 +0200

    working

--- model/quote.go
+++ model/quote.go
@@ -1,7 +1,7 @@
 package model
 
 type Quote struct {
-	ID       int    `json:"id,omitempty"`
+	ID       int    `json:"id"`
 	Title    string `json:"title,omitempty"`
 	Content  string `json:"content,omitempty"`
 	FilePath string `json:"file_path,omitempty"`
